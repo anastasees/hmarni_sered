79d938d97d011a6dca384dd0b166256e81d54396
anast <anast@ANASTASIA>
2026-02-24 11:34:26 +0200

    Add project files.

---
+++ lab1/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+using lab1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace lab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            throw new Exception("An error for lab.");
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
